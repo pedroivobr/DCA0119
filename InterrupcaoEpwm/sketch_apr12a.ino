f5ca09c168fe9029fcb5f1441229123a8a3c711d
Pedro Ivo <pedrounix@outlook.com.br>
2018-04-30 14:55:24 -0300

    UPDATE INTERRUPT
    atualização da interrupção.

--- InterrupcaoEpwm/sketch_apr12a.ino
+++ InterrupcaoEpwm/sketch_apr12a.ino
@@ -8,38 +8,21 @@ volatile uint8_t cor;
 //unsigned char
 ISR(TIMER1_OVF_vect){
   overflow_cont++;
-  Serial.println(OCR0A);
+  //Serial.println(OCR0A);
   if(overflow_cont < 41){
-    PORTB &= 0x00;
-    PORTB |= 0b00000001;
     OCR0A = OCR0A + 3;
   }
-  else if(overflow_cont < 49){
-    PORTB &= 0x00;
-    PORTB |= 0b00000010;
-  }
-  else if(overflow_cont < 61){
-    PORTB &= 0x00;
-    PORTB |= 0b00000100;
+  else if( overflow_cont > 48 && overflow_cont < 61){
     OCR0A = OCR0A - 4;
   }
-  else if(overflow_cont < 81){
-    PORTB &= 0x00;
-    PORTB |= 0b00001000;
-  }
-  else if(overflow_cont < 101){
-    PORTB &= 0x00;
-    PORTB |= 0b00010000;
+  else if(overflow_cont > 80 && overflow_cont < 101){
     OCR0A = OCR0A + 6;
   }
-  else if(overflow_cont < 120){
-    PORTB &= 0x00;
-    PORTB |= 0b00100000;
+  else if(overflow_cont > 101 && overflow_cont < 120){
     OCR0A = OCR0A - 10;
   }
-  else{ 
+  else if(255 > overflow_cont && overflow_cont > 120){
     OCR0A = 0;
-    //mantem
   }
   
 }
@@ -47,26 +30,56 @@ ISR(TIMER1_OVF_vect){
 int main(void)
 {
   Serial.begin(9600);
-  DDRB |= 0xFF;
-  PORTB |= 0x00;
-  PINB &= 0x00;
-
-  DDRD |= _BV(PD6);
+  //DDRB |= 0xFF;
+  PORTB &= 0x00;
+  DDRD &= 0x00;
+  DDRD |= 0b01001100;
+  PORTD &= 0x00;
+  //DDRD |= _BV(PD6);
   
   TCCR1A |= 0x00;
   TCCR1B &= 0x00;
   TCCR1B |= 0b00000011;
 
-  TCCR0A |= _BV(COM0A1) | _BV(WGM00) | _BV(WGM01);
-  TCCR0B |= _BV(CS00);
+  
+  TCCR0A |= 0b10000011;
+  TCCR0B |= 0b00000100;
   
   TIMSK1 &= 0x00;
   TIMSK1 |= 0b00000001;
 
   OCR0A = 0x00;
-  
+
+  //ligando motor
+  PORTD |= 0b00000100;
   sei();      
     while (1) 
-    {;}
+    {
+      Serial.println(OCR0A);
+      if(overflow_cont < 41){
+        PORTB &= 0x00;
+        PORTB |= 0b00100000;
+      }
+      else if(overflow_cont < 49){
+        PORTB &= 0x00;
+        PORTB |= 0b00010000;
+      }
+      else if(overflow_cont < 61){
+        PORTB &= 0x00;
+        PORTB |= 0b00001000;
+      }
+      else if(overflow_cont < 81){
+        PORTB &= 0x00;
+        PORTB |= 0b00000100;
+      }
+      else if(overflow_cont < 101){
+        PORTB &= 0x00;
+        PORTB |= 0b00000010;
+      }
+      else if(overflow_cont < 120){
+        PORTB &= 0x00;
+        PORTB |= 0b00000001;
+      }    
+    }
   return 0;
 }

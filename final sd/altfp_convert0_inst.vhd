a30cfcab77919414645d48c035990c8bacba62a0
Pedro Ivo <pedrounix@outlook.com.br>
2018-07-04 11:10:48 -0300

    sem sensor atmega

---
+++ final sd/altfp_convert0_inst.vhd
@@ -0,0 +1,5 @@
+altfp_convert0_inst : altfp_convert0 PORT MAP (
+		clock	 => clock_sig,
+		dataa	 => dataa_sig,
+		result	 => result_sig
+	);
---
+++ final sd/altfp_mult0_inst.vhd
@@ -0,0 +1,6 @@
+altfp_mult0_inst : altfp_mult0 PORT MAP (
+		clock	 => clock_sig,
+		dataa	 => dataa_sig,
+		datab	 => datab_sig,
+		result	 => result_sig
+	);
